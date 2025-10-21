25047f6e8618e5613e4f1f91e79cdf64e2d8285e
gomuang <gomuang12@gmail.com>
2025-10-20 19:09:18 -0500

    Update Admin User View
    Added list display with the custom group, search field, filter and ordering

--- LRAT_APP/DATABASE_APP/admin.py
+++ LRAT_APP/DATABASE_APP/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Software, SoftwareOption, Subscription
 # Register your models here.
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("group_names",)
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = ("groups", "is_staff", "is_superuser", "is_active")
+    ordering = ("last_name",)
+
+    @admin.display(description="Groups")
+    def group_names(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+
 admin.site.register(Software)
 admin.site.register(SoftwareOption)
 admin.site.register(Subscription)
