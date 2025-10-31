de9315205c768ad89ebbab6d8c0714d484c7a344
TommyM2 <92954351+TommyM2@users.noreply.github.com>
2025-10-31 11:19:26 -0500

    Update models.py

--- LRAT_APP/LRAT_APP/models.py
+++ LRAT_APP/LRAT_APP/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserDashboard(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_logins = models.IntegerField(default=0)
+    last_login_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
 
